beaf35d69ec51e8258d8f1bea5242074cfadb0d7
P. Mergey <gzm@gizmecano.net>
2020-07-17 07:20:17 +0200

    Prepare site name value

--- layout/head.php
+++ layout/head.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title></title>
+  <title><?php echo $site['name']; ?></title>
   <!-- Implement independent components -->
   <link rel="stylesheet" href="assets/knacss/knacss.css?version=7.1.3" media="all">
   <link rel="stylesheet" href="assets/inter/inter.css?version=3.13" media="all">
--- layout/required/site-data.php
+++ layout/required/site-data.php
@@ -4,3 +4,9 @@ $site['lang'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 
 /* Require custom functions */
 require_once 'layout/reusable/functions.php';
+
+/* Decode composer notations */
+$composer = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/composer.json'), true);
+
+/* Prepare variables based on composer notations */
+$site['name'] = ucfirst(substr($composer['name'], -9));
