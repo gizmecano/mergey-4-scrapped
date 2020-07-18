9ca5c47875317e30b81e6d39c1c20a12b62f29e9
P. Mergey <gzm@gizmecano.net>
2020-07-18 09:25:12 +0200

    Add footer parts inclusions

--- layout/footer.php
+++ layout/footer.php
@@ -4,6 +4,7 @@
   ?>
     <section id="footer-part-<?php echo $part; ?>" class="fl w25 small-w100 tiny-w100 man pal">
       <h3><?php echo ucfirst($glossary['foot']['part-' . $part]); ?></h3>
+      <?php include_once('parceled/footer-part-' . $part . '.php'); ?>
     </section>
   <?php
   }
---
+++ layout/parceled/footer-part-4.php
@@ -0,0 +1,6 @@
+<address itemscope itemtype="http://schema.org/Person">
+  <?php
+  include $_SERVER['DOCUMENT_ROOT'] . '/layout/reusable/div-itemprop-name.php';
+  include $_SERVER['DOCUMENT_ROOT'] . '/layout/reusable/div-itemprop-address.php';
+  ?>
+</address>
