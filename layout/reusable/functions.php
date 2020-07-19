4f7a0f8928a53cffe6df76ed4ef99d5c70e73b93
P. Mergey <gzm@gizmecano.net>
2020-07-19 13:45:07 +0200

    Add function for formatting a full text file

--- layout/reusable/functions.php
+++ layout/reusable/functions.php
@@ -1,12 +1,41 @@
 <?php
+/* Involve general namespaces */
+
+use Michelf\MarkdownExtra;
+use Michelf\SmartyPantsTypographer;
+
 /* Random picture
    Code adapted from: "How to get random image from directory using PHP"
    https://stackoverflow.com/q/1761252/4094098
  */
+
 function mrg_pict_random($path)
 {
   $find = glob($path . '*.{jpg,png}', GLOB_BRACE);
   $pick = $find[array_rand($find)];
   return $pick;
   unset($path, $find, $pick);
+}
+
+/* Full text
+   Parse and format entire Markdown file
+   with MarkdownExtra and SmartyPants Typographer
+   SmartyPants Configuration options
+   https://michelf.ca/projects/php-smartypants/configuration/
+ */
+function mrg_text_full($path, $lang)
+{
+  $file = file_get_contents($path);
+  $text = MarkdownExtra::defaultTransform($file);
+  switch ($lang) {
+    case 'fr':
+      $options = "qDe:+;+m+t+";
+      break;
+    case 'en':
+      $options = "qDet+";
+      break;
+  }
+  $text = SmartyPantsTypographer::defaultTransform($text, $options);
+  return $text;
+  unset($lang, $path, $text);
 }
