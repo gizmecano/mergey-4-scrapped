77b6ece2507480c11af8809508e2579174b6d34d
P. Mergey <gzm@gizmecano.net>
2020-07-20 09:20:11 +0200

    Update address item properties for imprint case

--- layout/reusable/div-itemprop-address.php
+++ layout/reusable/div-itemprop-address.php
@@ -1,4 +1,12 @@
 <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+  <?php
+  if(pathinfo(debug_backtrace()[0]['file'])['basename'] === 'proof-imprint.php') { ?>
+    <p>
+      <span itemprop="streetAddress"><?php echo $masthead['pa']['sa']; ?></span>
+    </p>
+  <?php
+  }
+  ?>
   <p>
     <span itemprop="postalCode"><?php echo $masthead['pa']['pc']; ?></span>
     <span itemprop="addressLocality"><?php echo $masthead['pa']['al']; ?></span>
