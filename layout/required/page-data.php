bb9ef1cd7339e9d93ef896170f2b9eb202a8562f
P. Mergey <gzm@gizmecano.net>
2020-07-20 08:45:12 +0200

    Update metadata composition according to page type

--- layout/required/page-data.php
+++ layout/required/page-data.php
@@ -9,3 +9,16 @@ if (empty($page['lang'])) {
 
 /* Decode languages glossaries */
 $glossary = json_decode(file_get_contents('speech/idioms/' . $page['lang'] . '/glossary.json'), true);
+
+/* Compose specific metadata according to page type */
+if(isset($page['type'])) {
+  $page['name'] = ucfirst($glossary['page'][$page['body']][$page['type']]);
+}
+
+switch ($page['body']) {
+  case 'index':
+    $meta['title'] = $site['name'];
+    break;
+  default:
+    $meta['title'] = $page['name'] . ' &#8226; ' . $site['name'];
+}
