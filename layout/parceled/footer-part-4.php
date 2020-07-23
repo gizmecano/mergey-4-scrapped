bb59230c148ed2a2d33ec827790f8c02df52e83d
P. Mergey <gzm@gizmecano.net>
2020-07-23 11:00:18 +0200

    Add links list for proof layout in footer

--- layout/parceled/footer-part-4.php
+++ layout/parceled/footer-part-4.php
@@ -4,3 +4,8 @@
   include $_SERVER['DOCUMENT_ROOT'] . '/layout/reusable/div-itemprop-address.php';
   ?>
 </address>
+
+<ul>
+  <li><a href="/proof/contact"><?php echo ucfirst($glossary['page']['proof']['contact']); ?></a></li>
+  <li><a href="/proof/imprint"><?php echo ucfirst($glossary['page']['proof']['imprint']); ?></a></li>
+</ul>
