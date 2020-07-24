24bbdb4cfb5f647ec6d00a1bcb1099551fbd7e62
P. Mergey <gzm@gizmecano.net>
2020-07-24 14:10:33 +0200

    Add a list for accessory informations in footer

--- layout/parceled/footer-part-4.php
+++ layout/parceled/footer-part-4.php
@@ -9,3 +9,12 @@
   <li><a href="/contact"><?php echo ucfirst($glossary['page']['proof']['contact']); ?></a></li>
   <li><a href="/imprint"><?php echo ucfirst($glossary['page']['proof']['imprint']); ?></a></li>
 </ul>
+
+<ul class="u-small">
+  <li>2010&#8211;<?php echo date('Y'); ?></li>
+  <li>
+    <a href="<?php echo $composer['homepage']; ?>">
+      <?php echo ucfirst($glossary['site']['build']); ?>
+    </a>
+  </li>
+</ul>
