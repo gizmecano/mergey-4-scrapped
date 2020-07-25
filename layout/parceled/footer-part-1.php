76c540f73dfe69d9d588c806c06e368785828106
P. Mergey <gzm@gizmecano.net>
2020-07-25 14:50:07 +0200

    Add section for stall content in footer

---
+++ layout/parceled/footer-part-1.php
@@ -0,0 +1,12 @@
+<ul>
+  <li>
+    <a href="/activities">
+      <?php echo ucfirst($glossary['page']['stall']['activities']); ?>
+    </a>
+  </li>
+  <li>
+    <a href="/principles">
+      <?php echo ucfirst($glossary['page']['stall']['principles']); ?>
+    </a>
+  </li>
+</ul>
