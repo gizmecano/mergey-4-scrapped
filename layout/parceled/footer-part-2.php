d5193acc8bafc75ce8c9a130419893652c137c19
P. Mergey <gzm@gizmecano.net>
2020-07-25 15:05:06 +0200

    Add section devoted to side projects in footer

---
+++ layout/parceled/footer-part-2.php
@@ -0,0 +1,17 @@
+<ul>
+  <li>
+    <a href="http://clepsydre.free.fr/">
+      La Clepsydre
+    </a>
+  </li>
+  <li>
+    <a href="http://gizmecano.net/">
+      the GizMecano Laboratory
+    </a>
+  </li>
+  <li>
+    <a href="http://mno.gizmecano.net/">
+      Miscellanées numériques d’un ours
+    </a>
+  </li>
+</ul>
