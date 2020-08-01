2248fa0c89e88bea7ef1655178344a468776893c
P. Mergey <gzm@gizmecano.net>
2020-08-01 14:15:32 +0200

    Add OpenGraph properties

---
+++ layout/required/meta-data.php
@@ -0,0 +1,7 @@
+<!-- OpenGraph properties -->
+<meta property="og:locale" content="fr_CH">
+<meta property="og:type" content="website">
+<meta property="og:url" content="<?php echo $meta['canon']; ?>">
+<meta property="og:title" content="<?php echo $meta['title']; ?>">
+<meta property="og:description" content="<?php echo $meta['blurb']; ?>">
+<meta property="og:site_name" content="<?php echo $site['name']; ?>">
