2e4d181c6ca2694c877bca07b0de1c17bde0b2db
P. Mergey <gzm@gizmecano.net>
2020-08-04 13:35:16 +0200

    Fix minor details in header for index layout

--- index.php
+++ index.php
@@ -19,10 +19,12 @@ $page['path'] = 'speech/' . $page['lang'] . '/' . $page['body'];
     <?php require_once 'layout/header.php'; ?>
     <main id="main" class="w75 small-w100 tiny-w100 man pal">
       <article class="w100 small-w100 tiny-w100 fr">
-        <header class="w75 small-w100 tiny-w100 fl">
+        <header class="w100 small-w100 tiny-w100">
           <h2 class="visually-hidden"><?php echo ucfirst($glossary['page']['index']['hidden']); ?></h2>
           <?php echo mrg_text_full($page['path'] . '-motto.md', $page['lang']); ?>
-          <img src="<?php echo $meta['image']; ?>" alt="<?php echo $meta['blurb']; ?>">
+          <div class="w50 small-w100 tiny-w100">
+            <img class="w100 small-w100 tiny-w100 center" src="<?php echo $meta['image']; ?>" alt="<?php echo $meta['blurb']; ?>">
+          </div>
           <?php echo mrg_text_full($page['path'] . '-axiom.md', $page['lang']);
           ?>
         </header>
