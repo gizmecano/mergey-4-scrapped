94ed3c56e0709c9a4139d611869f756a212a0cd1
P. Mergey <gzm@gizmecano.net>
2020-08-27 09:00:17 +0200

    Add source code repository data in imprint page

--- layout/parceled/proof-imprint.php
+++ layout/parceled/proof-imprint.php
@@ -24,3 +24,20 @@
     <p>(<span itemprop="addressCountry"><?php echo $glossary['code']['iso-1'][$masthead['ha']['ac']]; ?></span>)</p>
   </div>
 </address>
+
+<address class="w50 small-w100 tiny-w100 fr" itemscope itemtype="http://schema.org/Organization">
+  <h3 itemprop="potentialAction"><?php echo ucfirst($glossary['host']['source']); ?></h3>
+  <div itemprop="name">
+    <p><a itemprop="url" href="https://github.com/"><?php echo $masthead['sa']['ln']; ?></a></p>
+  </div>
+  <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+    <p><span itemprop="streetAddress"><?php echo $masthead['sa']['sa']; ?></span></p>
+    <p>
+      <span itemprop="postalCode"><?php echo $masthead['sa']['pc']; ?></span>
+      <span itemprop="addressLocality"><?php echo $masthead['sa']['al']; ?></span>
+    </p>
+    <p>
+      <span itemprop="addressRegion"><?php echo $glossary['code']['iso-2'][$masthead['sa']['ar']]; ?></span>
+      (<span itemprop="addressCountry"><?php echo $glossary['code']['iso-1'][$masthead['sa']['ac']]; ?></span>)</p>
+  </div>
+</address>
